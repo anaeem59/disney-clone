149907a63b3af2fbbb36c6109ec288648a816db2
github_anaeem59 <anaeem59@gmail.com>
2021-05-05 17:26:21 +0530

    Day Two

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,79 @@
+import { useDispatch, useSelector } from 'react-redux'
 import styled from 'styled-components'
+import ImageSlider from './ImageSlider'
+import NewDisney from './NewDisney'
+import Originals from './Originals'
+import Recommends from './Recommends'
+import Trending from './Trending'
+import Viewers from './Viewers'
+import { selectUserName } from '../features/user/userSlice'
+import { useEffect } from 'react'
+import db from '../firebase'
+import { setMovies } from '../features/movie/movieSlice'
 
 const Home = (props) => {
+    const dispatch = useDispatch();
+    const username = useSelector(selectUserName);
+    let recommends= [];
+    let newDisney= [];
+    let original= [];
+    let trending= [];
+
+    useEffect(() => {
+        db.collection('movies').onSnapshot((snapshot) => {
+            snapshot.docs.map((doc) => {
+                switch(doc.data().type) {
+                    case 'recommend':
+                        recommends = [...recommends, {id: doc.id, ...doc.data()}]
+                    break;
+                    case 'new':
+                        newDisney = [...newDisney, {id: doc.id, ...doc.data()}]
+                    break;
+                    case 'original':
+                        original = [...original, {id: doc.id, ...doc.data()}]
+                    break;
+                    case 'trending':
+                        trending = [...trending, {id: doc.id, ...doc.data()}]
+                    break;
+                }
+            })
+        dispatch(setMovies({
+            recommend: recommends,
+            newDisney: newDisney,
+            original: original,
+            trending: trending,
+        }))
+    })
+    }, [username])
+
     return (
-        <div>Home</div>
+        <Container>
+            <ImageSlider />
+            <Viewers />
+            <Recommends />
+            <NewDisney />
+            <Originals />
+            <Trending />
+        </Container>
     )
 }
+
+const Container = styled.main`
+    position: relative;
+    min-height: calc(100vh - 250px);
+    overflow-x: hidden;
+    display: block;
+    top: 72px;
+    padding: 0 calc(3.5vw - 5px);
+
+    &:after {
+        background: url('/images/home-background.png') center center / cover no-repeat fixed;
+        content: '';
+        position: absolute;
+        inset: 0px;
+        opacity: 1;
+        z-index: -1;
+    }
+`;
 
 export default Home
